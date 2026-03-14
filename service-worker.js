7468a49bc985b820bb186d8af232f0fcca75ae4e
obaidulmostafa100 <obaidulmostafa100@gmail.com>
2026-03-14 13:44:48 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,34 @@
 self.addEventListener("install", function(e) {
   console.log("Service Worker Installed");
 });
+const CACHE_NAME = "shane-hadiye-zaman-v1";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/manifest.json"
+];
+
+self.addEventListener("install", function(event) {
+event.waitUntil(
+caches.open(CACHE_NAME).then(function(cache) {
+return cache.addAll(urlsToCache);
+})
+);
+});
+
+self.addEventListener("fetch", function(event) {
+event.respondWith(
+caches.match(event.request).then(function(response) {
+if (response) {
+return response;
+}
+return fetch(event.request).then(function(networkResponse) {
+return caches.open(CACHE_NAME).then(function(cache) {
+cache.put(event.request, networkResponse.clone());
+return networkResponse;
+});
+});
+})
+);
+});
